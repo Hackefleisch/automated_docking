b6947232800f2c4b66d314ebd26f7bbcf5233e61
Paul Eisenhuth <eisenhuth@informatik.uni-leipzig.de>
2025-05-19 13:58:00 +0200

    Updated constraint code and made it usable

--- ligand_docking_cst.py
+++ ligand_docking_cst.py
@@ -89,13 +89,17 @@ def rdkit_to_mutable_res(mol):
     lig_restype.autodetermine_chi_bonds()
 
     index_to_name = {}
+    pos_to_name = {}
 
     for idx in range( mol.GetNumAtoms() ):
         vd = index_to_vd[idx]
         atm_name = lig_restype.atom_name( vd )
+        atom_pos = conf.GetAtomPosition( idx )
+        binned_pos = ( int(atom_pos.x * 10), int(atom_pos.y * 10), int(atom_pos.z * 10) )
         index_to_name[ idx ] = atm_name
+        pos_to_name[ binned_pos ] = atm_name
 
-    return lig_restype, index_to_vd, index_to_name
+    return lig_restype, index_to_vd, index_to_name, pos_to_name
 
 def add_confs_to_res(mol, mutable_res, index_to_vd):
 
@@ -124,31 +128,34 @@ def prepare_pose( smiles, pose, ref_mol ):
 
     # Determine number of different conformers based on molecule flexibility
     nrotbonds = Lipinski.NumRotatableBonds(new_mol)
-    nconf = 49
+    nconf = 50
     if nrotbonds > 12:
-        nconf = 299
+        nconf = 300
     elif nrotbonds >= 8:
-        nconf = 199
+        nconf = 200
     new_mol = Chem.AddHs( new_mol )
 
     # Align new Mol with reference Mol and generate conformers
-    AllChem.ConstrainedEmbed( new_mol, ref_mol )
-    AllChem.EmbedMultipleConfs(new_mol, numConfs=nconf, clearConfs = False, maxAttempts = 30)
-    AllChem.AlignMolConformers(new_mol)
+    if not new_mol.HasSubstructMatch(ref_mol):
+        print('ERROR: New mol has no substructure match with reference mol')
+        return
+    for i in range(nconf):
+        AllChem.ConstrainedEmbed( new_mol, ref_mol, clearConfs = False, coreConfId=0, randomseed=42+i )
+    AllChem.MMFFOptimizeMoleculeConfs(new_mol, numThreads=0)
+    match = new_mol.GetSubstructMatch(ref_mol)
+    atom_map = list(zip(match, range(ref_mol.GetNumAtoms())))
+
+    for cid in range(new_mol.GetNumConformers()):
+        AllChem.AlignMol( new_mol, ref_mol, atomMap = atom_map, prbCid=cid )
+
+    #with Chem.SDWriter('constraints/rdkit_conf.sdf') as w:
+    #    for cid in range(new_mol.GetNumConformers()):
+    #        w.write(new_mol, confId=cid)
 
     # Turn RDKit Mol into Rosetta Residue
-    mutres, index_to_vd, index_to_name = rdkit_to_mutable_res( new_mol )
+    mutres, index_to_vd, index_to_name, pos_to_name = rdkit_to_mutable_res( new_mol )
     mutres = add_confs_to_res( new_mol, mutres, index_to_vd )
     res = mutable_res_to_res(mutres)
-
-    # Examplified creation of 2D image with Rosetta atom names
-    d = rdMolDraw2D.MolDraw2DCairo(750, 750) # or MolDraw2DSVG to get SVGs
-    for i in range( new_mol.GetNumAtoms() ):
-        new_mol.GetAtomWithIdx(i).SetProp('atomNote', index_to_name[i])
-    AllChem.Compute2DCoords(new_mol)
-    d.DrawMolecule(new_mol)
-    d.FinishDrawing()
-    d.WriteDrawingText('atom_map.png')
 
     # copy receptor pose (should not contain a ligand!)
     new_pose = pyrosetta.rosetta.core.pose.Pose()
@@ -159,22 +166,39 @@ def prepare_pose( smiles, pose, ref_mol ):
     new_pose.pdb_info().chain( new_pose.total_residue(), 'X' )
     new_pose.update_pose_chains_from_pdb_chains()
 
-    return new_pose, new_mol
+    return new_pose, new_mol, pos_to_name
 
-def add_constraints( pose, constraints ):
+def find_closest( pos, pos_to_name ):
+    min_dist = 99999999.9
+    min_pos = None
+    for ref_pos in pos_to_name:
+        d_sq = 0.0
+        for i in range(len(pos)):
+            d_sq += (pos[i] - ref_pos[i])**2
+        if d_sq < min_dist:
+            min_dist = d_sq
+            min_pos = ref_pos
+
+    print('Mapped', pos, 'to', min_pos, 'with minimal distance', min_dist, '- returning', pos_to_name[min_pos])
+    return pos_to_name[min_pos]
+
+def add_constraints( pose, constraints, pos_to_name ):
 
     for constraint in constraints:
         # Receptor atom selected via Residue index and atom name
         atom1 = pyrosetta.rosetta.core.id.AtomID( pose.residue( constraint[ 0 ] ).type().atom_index( constraint[ 1 ] ), constraint[ 0 ] )
         # ligand atom selected from last residue (aka ligand residue) and atom name
-        atom2 = pyrosetta.rosetta.core.id.AtomID( pose.residue( pose.total_residue() ).type().atom_index( constraint[ 2 ] ), pose.total_residue() )
+        pos = ( int(constraint[ 2 ][0] * 10), int(constraint[ 2 ][1] * 10), int(constraint[ 2 ][2] * 10) )
+        name = find_closest( pos, pos_to_name )
+        atom2 = pyrosetta.rosetta.core.id.AtomID( pose.residue( pose.total_residue() ).type().atom_index( name ), pose.total_residue() )
         harmonic = pyrosetta.rosetta.core.scoring.func.HarmonicFunc( constraint[3], constraint[4] )
         cst = pyrosetta.rosetta.core.scoring.constraints.AtomPairConstraint( atom1, atom2, harmonic )
         pose.add_constraint(cst)
 
 def dock( pose, all_results, mover, scfx, repeats=1 ):
 
-    for _ in range(repeats):
+    for dock_idx in range(repeats):
+        print('Start docking', dock_idx+1, 'of', repeats)
         work_pose = pyrosetta.rosetta.core.pose.Pose()
         work_pose.detached_copy(pose)
         mover.apply( work_pose )
@@ -202,15 +226,36 @@ def dock( pose, all_results, mover, scfx, repeats=1 ):
         results[ 'pairwise_energy' ] = idelta_scores[ 'if_X_fa_pair' ]
 
         all_results.append( results )
+    print('Finished all dockings')
 
 def analyze_results( results, rdkit_mol, max_cst_penalty ):
+
+    filtered_results = []
+    cst_penalty_violations = 0
+    delta_g_violations = 0
+    for result in results:
+        keep = True
+        if result['atom_pair_cst'] > max_cst_penalty:
+            keep = False
+            cst_penalty_violations += 1
+        if result[ 'delta_g' ] > 0.0:
+            keep = False
+            delta_g_violations += 1
+        if keep:
+            filtered_results.append( result )
+    print( len(filtered_results), 'of', len(results), 'results were successful.' )
+    print( cst_penalty_violations, 'results were discarde due to constraint penalties above', max_cst_penalty )
+    print( delta_g_violations, 'results were discarde due to positive delta gs' )
+    if len( filtered_results ) == 0:
+        print('ERROR: No successful results. Skipping analysis.')
+        return
 
     best_pose = None
     best_score = 999999.99
     best_index = 0
 
-    for idx, result in enumerate(results):
-        if result[ 'delta_g' ] < best_score and result['atom_pair_cst'] < max_cst_penalty:
+    for idx, result in enumerate(filtered_results):
+        if result[ 'delta_g' ] < best_score:
             best_score = result[ 'delta_g' ]
             best_pose = result[ 'pose' ]
             best_index = idx
@@ -220,13 +265,13 @@ def analyze_results( results, rdkit_mol, max_cst_penalty ):
     rmsd_calc.set_residue_selector(res_selector)
     rmsd_calc.set_comparison_pose( best_pose )
 
-    for result in results:
+    for result in filtered_results:
         rmsd = rmsd_calc.calculate( result[ 'pose' ] )
         result[ 'rmsd' ] = rmsd
 
     x = []
     y = []
-    for result in results:
+    for result in filtered_results:
         rmsd = result[ 'rmsd' ]
         score = result[ 'delta_g' ]
         x.append( rmsd )
@@ -235,12 +280,12 @@ def analyze_results( results, rdkit_mol, max_cst_penalty ):
     plt.scatter( x, y )
     plt.ylabel( 'Delta G (REU)' )
     plt.xlabel( 'RMSD' )
-    plt.savefig( 'funnel.png' )
+    plt.savefig( 'constraints/funnel.png' )
 
     best_pose.dump_pdb("constraints/best_pose.pdb")
 
     header = [ 'name' ]
-    for key in results[0].keys():
+    for key in filtered_results[0].keys():
         if key != 'pose':
             header.append( key )
     padding = 16
@@ -255,42 +300,47 @@ def analyze_results( results, rdkit_mol, max_cst_penalty ):
     print()
 
     print( "".join([h.rjust( padding ) for h in header]) )
-    print( 'Best'.rjust( padding ), ''.join( f'{results[best_index][h]:.4f}'.rjust( padding ) for h in header[1:] ), sep='' )
+    print( 'Best'.rjust( padding ), ''.join( f'{filtered_results[best_index][h]:.4f}'.rjust( padding ) for h in header[1:] ), sep='' )
 
-    for i in range( len(results) ):
-        print( str(i+1).rjust( padding ), ''.join( f'{results[i][h]:.4f}'.rjust( padding ) for h in header[1:] ), sep='' )
+    for i in range( len(filtered_results) ):
+        filtered_results[i]['pose'].dump_pdb("constraints/result_" + str(i+1) + ".pdb")
+        print( str(i+1).rjust( padding ), ''.join( f'{filtered_results[i][h]:.4f}'.rjust( padding ) for h in header[1:] ), sep='' )
 
 def main():
 
-    pybel_mol = next(pybel.readfile("sdf", 'constraints/relax.sdf'))
+    pybel_mol = next(pybel.readfile("sdf", 'constraints/ref_ligand_core.sdf'))
     pybel_sdf = pybel_mol.write('sdf')
 
     # RDKits sanitization step automatically detects aromaticity
     # I am assuming that it is best to provide molecules in kekulized form with all hydrogens present
     ref_mol = Chem.MolFromMolBlock( pybel_sdf )
 
-    pose = pyrosetta.pose_from_pdb( "constraints/relax_nolig.pdb" )
+    pose = pyrosetta.pose_from_pdb( "constraints/7f83_relax.pdb" )
 
-    smiles = 'COc5cc(OCc1ccncc1)cc6nc(c4cc(CCc2ccnnc2)c(OCCO)c(Cc3cnccn3)c4)[nH]c(=O)c56'
-    lig_pose, new_mol = prepare_pose( smiles, pose, ref_mol )
+    #smiles = 'O=C(c2c1cnccc1c4NCCc3cncc2c34)N5CCC8(CC5)CN(c7ccc6ccncc6c7)C8'
+    smiles = 'CN(C(=O)CN3CC2(CCN(C(=O)c1cccnc1)CC2)C3)c5ccc4COCc4c5'
+    lig_pose, new_mol, pos_to_name = prepare_pose( smiles, pose, ref_mol )
 
     constraints = [
-        [92, 'HE2', 'O5', 2.5, 0.25],
-        [45, 'HH', 'N1', 2.5, 0.25],
-        [87, 'OH', 'O4', 2.5, 0.25],
+        [364,  'HG', [-6.7520, -0.1555, 13.0855], 1.80, 0.125],
+        [ 65, 'OD2', [-7.1638,  5.8368, 16.5862], 3.23, 0.25],
+        [ 65, 'OD2', [-7.5181,  3.1143, 15.5623], 3.25, 0.25],
+        [ 89,  'CB', [-6.0966,  5.3594, 15.7673], 3.70, 0.25],
+        [ 86,  'CD', [-7.1638,  5.8368, 16.5862], 5.11, 0.50],
     ]
 
-    add_constraints( lig_pose, constraints )
+    add_constraints( lig_pose, constraints, pos_to_name )
 
     protocol_xml = pyrosetta.rosetta.protocols.rosetta_scripts.XmlObjects.create_from_file("constraints/transform_std.xml")
     mover = protocol_xml.get_mover("ParsedProtocol")
     scfx = protocol_xml.get_score_function("hard_rep")
 
     results = []
-    dock( lig_pose, results, mover, scfx, repeats=150 )
+    dock( lig_pose, results, mover, scfx, repeats=300 )
 
-    analyze_results( results, new_mol, max_cst_penalty=10 )
+    analyze_results( results, new_mol, max_cst_penalty=15 )
 
 
 if __name__ == "__main__":
+
     main()
